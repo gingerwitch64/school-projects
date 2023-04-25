0b4022932f48a2ab92b2eb8b3560f16cbe0bb043
Nola G <redpendrew@gmail.com>
2023-04-25 10:24:52 -0400

    pygame project: rotation is working

--- pygame-project/__main__.py
+++ pygame-project/__main__.py
@@ -1,4 +1,5 @@
 import math
+from math import sin, cos, radians
 import pygame
 from pygame.locals import *
 pygame.init()
@@ -16,13 +17,39 @@ def clamp(num, min_value, max_value):
 def delta_line(coord1,coord2):
     return abs(math.sqrt(((coord2[0]-coord1[0])*(coord2[0]-coord1[0]))+((coord2[1]-coord1[1])*(coord2[1]-coord1[1]))))
 
+"""Credit goes this amazing person on stackoverflow: https://stackoverflow.com/a/45511474"""
+def rotatePolygon(polygon, degrees):
+    """ Rotate polygon the given angle about its center. """
+    theta = radians(degrees)  # Convert angle to radians
+    cosang, sinang = cos(theta), sin(theta)
+
+    points = polygon
+    # find center point of Polygon to use as pivot
+    n = len(points)
+    cx = sum(p[0] for p in points) / n
+    cy = sum(p[1] for p in points) / n
+
+    new_points = []
+    for p in points:
+        x, y = p[0], p[1]
+        tx, ty = x-cx, y-cy
+        new_x = ( tx*cosang + ty*sinang) + cx
+        new_y = (-tx*sinang + ty*cosang) + cy
+        new_points.append([new_x, new_y])
+
+    return new_points
+
 active = True
-rotating_square_points = [[0,0],
-                          [0,400],
-                          [400,400],
-                          [400,0]]
+rotating_square_length = 400
+rotating_square_points = [
+    [(display_width-rotating_square_length)/2,(display_height-rotating_square_length)/2],
+    [(display_width+rotating_square_length)/2,(display_height-rotating_square_length)/2],
+    [(display_width+rotating_square_length)/2,(display_height+rotating_square_length)/2],
+    [(display_width-rotating_square_length)/2,(display_height+rotating_square_length)/2]
+]
 entities = []
-inc = 0.1
+rotation = 0
+inc = 0.01
 
 while active:
     # Quit checker
@@ -30,18 +57,10 @@ while active:
         if event.type == pygame.QUIT:
             active = False
     
+    rotation += inc
+    if rotation >= 360:
+        rotation = 0
     pygame.draw.rect(display,DARKGR,(0,0,display_width,display_height))
-
-    pygame.draw.polygon(display,WHITE,rotating_square_points)
-    rotating_square_points[0][0] += inc
-    rotating_square_points[0][1] -= inc
-    rotating_square_points[1][0] -= inc
-    rotating_square_points[1][1] -= inc
-    """rotating_square_points[3][0] += inc
-    rotating_square_points[3][1] += inc
-    rotating_square_points[2][0] -= inc
-    rotating_square_points[2][1] += inc"""
-    print(rotating_square_points)
-    print(delta_line(rotating_square_points[0],rotating_square_points[1]))
+    pygame.draw.polygon(display,WHITE,rotatePolygon(rotating_square_points, rotation))
     pygame.display.update()
     
