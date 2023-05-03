4d018dc8efa248cbe2c6f285f975dfe07ace24f7
Nola G <nolag@n11.dev>
2023-05-02 21:53:49 -0400

    pygame project: bg bounds, debugging implemented

--- pygame-project/__main__.py
+++ pygame-project/__main__.py
@@ -4,8 +4,8 @@ from logic import *
 relpath = pathlib.Path(__file__).parent.resolve() # This allows the game to be run from any directory and not have issues finding assets.
 clock = pygame.time.Clock()
 pygame.init()
-display_width,display_height = 800,600
-display = pygame.display.set_mode((display_width,display_height))
+display = Window(800,600,None)
+display.surface = pygame.display.set_mode((display.x,display.y))
 pygame.display.set_caption("ZGSClone")
 framerate = 60
 
@@ -19,13 +19,12 @@ thermal_bg = pygame.image.load(f"{relpath}/assets/img/thermal-topdown.png") # Ne
 
 view = Coord(0,0)
 viewspeed = 5
-active = True
 entities = []
 
 last_time = time.time()
 pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0)) # Credit to Rockybilly via stackoverflow: https://stackoverflow.com/a/40628090/16863801
 
-
+active = True
 while active:
     dt = (time.time() - last_time) * framerate
     last_time = time.time()
@@ -44,12 +43,25 @@ while active:
         view.y -= viewspeed
     if pressedKeys[K_DOWN]:
         view.y += viewspeed
+    if pressedKeys[K_d]: # Debug - get current coords (plan to add other info as well)
+        print(view.x,view.y,dt)
+    if pressedKeys[K_g]: # Goto coords
+        goto = input("Input coords exactly as (excluding the quotation marks) \"x,y\":")
+        goto = goto.split(",")
+        if goto == "":
+            print("Nothing specified; passing")
+            pass
+        elif len(goto) != 2:
+            print(f"{len(goto)} parameters specified: requires 2")
+        else:
+            goto = Coord(int(goto[0]),int(goto[1]))
+            view.x = goto.x
+            view.y = goto.y
 
-    view.x = clamp(view.x,0,566)
-    view.y = clamp(view.y,0,280)
-    print(view.x,view.y)
+    view.x = clamp(view.x,0,thermal_bg.get_width()-display.x)
+    view.y = clamp(view.y,0,thermal_bg.get_height()-display.y)
     
-    pygame.draw.rect(display,MISPRP,(0,0,display_width,display_height))
-    pygame.Surface.blit(display,thermal_bg,(-view.x,-view.y))
+    pygame.draw.rect(display.surface,MISPRP,(0,0,display.x,display.y))
+    pygame.Surface.blit(display.surface,thermal_bg,(-view.x,-view.y))
     pygame.display.update()
     clock.tick(framerate)
