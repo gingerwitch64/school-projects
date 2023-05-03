948a7471fc60dc3755e68d2d0eeb0a868fe5905a
Nola G <nolag@n11.dev>
2023-05-02 21:53:03 -0400

    pygame project: clarify logic lib usage, add more

--- pygame-project/logic.py
+++ pygame-project/logic.py
@@ -1,3 +1,5 @@
+# This file is just for things like classes or functions that help make the structure of this project a bit more readable.
+
 class Coord:
     x = 0
     y = 0
@@ -5,6 +7,24 @@ class Coord:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+class Bounds:
+    min = 0
+    max = 0
+
+    def __init__(self, min, max) -> None:
+        self.min = min
+        self.max = max
+
+class Window:
+    x = 0
+    y = 0
+    surface = 0
+
+    def __init__(self, x, y, surface) -> None:
+        self.x = x
+        self.y = y
+        self.surface = surface
 
 def clamp(num, min_value, max_value):
     return max(min(num, max_value), min_value)
