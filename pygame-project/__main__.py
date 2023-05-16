52021af78ab917901057b5c5dda949bd8e82b25f
Nola G <nolag@n11.dev>
2023-05-15 23:44:14 -0400

    sync - added text, almost done!

--- pygame-project/__main__.py
+++ pygame-project/__main__.py
@@ -18,6 +18,8 @@ undead_walk_1 = pygame.image.load(f"{relpath}/assets/img/undead_walk_1.png")
 undead_walk_2 = pygame.image.load(f"{relpath}/assets/img/undead_walk_2.png")
 undead_fallen = pygame.image.load(f"{relpath}/assets/img/undead_fallen.png")
 
+special_elite = pygame.font.Font(f"{relpath}/assets/fonts/SpecialElite-Regular.ttf",24) # Load the font needed for the stats UI.
+
 CHAMBER_TIME = user.difficulty["gunship"]["firerate"]
 chambered = True
 ANIMATE = 750   # These are the event cooldowns in milliseconds.
@@ -39,6 +41,10 @@ pygame.time.set_timer(riseevent,RISETIME)
 view = Coord(0,0)
 viewspeed = user.preferences["viewspeed"]   # Note that this value is fetched from user.py
 entities = []
+score = 0 # Your score!
+basehp = user.difficulty["base"]["hp"]
+initbasehp = basehp
+waveinc = user.difficulty["undead"]["wavesizeinc"]
 
 # This is the first undead. Although I used this guy for testing purposes, I think I'll keep him here because there's not much use in removing him.
 entities.append(Undead(1111,642,undead_walk_1,randrange(0,20,1)/10,user.difficulty["undead"]["hp"],[(5,deviate([233],10))]))
@@ -71,21 +77,20 @@ while active:
         elif event.type == statusevent:
             for entity in entities:
                 if type(entity) == Undead:
-                    if entity.hp <= 0:      # Fells an undead if it's... dead.
-                        entity.state = undead_fallen
                     if entity.state == undead_walk_1 or entity.state == undead_walk_2:
                         if entity.x <= 0:   # Removes an undead when it has reached the bounds.
                             entities.remove(entity)
+                            score -= 50
                         destination = Coord(entity.path[len(entity.path)-1][0],entity.path[len(entity.path)-1][1])
                         entity.x -= user.difficulty["undead"]["speed"]
                         slope = (entity.inity-destination.y)/(entity.initx-destination.x)   # Unfortunately, as of right now, undead do not move across a line at a speed consistent relative to that line.
                         entity.y = (slope*(entity.x-destination.x))+destination.y           # Their x is changed and their y is set depending on it.
         elif event.type == waveevent:
             i = 0
-            wavesize = randrange(user.difficulty["undead"]["minwavesize"],user.difficulty["undead"]["maxwavesize"]) # Randomly determines wave size based on user.difficulty min and max
+            wavesize = randrange(user.difficulty["undead"]["minwavesize"]+waveinc,user.difficulty["undead"]["maxwavesize"]+waveinc) # Randomly determines wave size based on user.difficulty min, max and increase over time
             while i < wavesize:
                 entities.append(Undead(                     # Here is where the Undead() are created. It seems complex, but again, it's just tedious.
-                    deviate(1100,50), deviate(650,50),      # The deviate(a,b) function is from logic.py and randomly chooses a value between a-b and a+b.
+                    deviate(1100,90), deviate(650,90),      # The deviate(a,b) function is from logic.py and randomly chooses a value between a-b and a+b.
                     undead_rising, randrange(0,20,1)/10,    # State is set to risen (player has a bit of time to notice new undead), but that second variable (animation offset) is actually unused.
                     user.difficulty["undead"]["hp"],        # Undead hit points are set here from user.difficulty, and
                     [(5,deviate([233],10))]                 # these are the points of the path for the undead to travel. It is a list, but in reality I've only programmed them to use one point.
@@ -121,7 +126,10 @@ while active:
                 if type(entity) == Undead:
                     if type(entity.state) == pygame.surface.Surface:
                         if hitrect.colliderect(entity.state.get_rect(topleft=(entity.x,entity.y))):
-                            entity.hp = entity.hp - user.difficulty["gunship"]["dmg"]
+                            entity.hp -= user.difficulty["gunship"]["dmg"]
+                            if entity.hp <= 0 and entity.state != undead_fallen:
+                                score += 100
+                                entity.state = undead_fallen
 
     if user.DEBUG:
         if pressedKeys[K_d]: # Debug - get current coords (plan to add other info as well)
@@ -150,6 +158,9 @@ while active:
     for entity in entities:
         if type(entity) is Undead:
             pygame.Surface.blit(display.surface,entity.state,(entity.x-view.x,entity.y-view.y))
+
+    stats = special_elite.render(f"Base HP: {basehp}/{initbasehp} | Score: {score}",True,WHITE,None)
+    display.surface.blit(stats,(10,10))
     
     if True: # This was all written to de-obfuscate the process of drawing some lines. In short, coords are taken and drawn out a-coord-ingly (of course, pun intended).
         c = user.preferences["crosshair"]["length"]
--- pygame-project/user.py
+++ pygame-project/user.py
@@ -7,10 +7,10 @@ preferences = {
     "framerate": 60, # Frames per second target for the game.
     "viewspeed": 5, # How fast the view moves.
     "crosshair": {
-        "length": 10, # Length of the perpendicular crosshairs in pixels.
+        "length": 8, # Length of the perpendicular crosshairs in pixels.
         "multiplier": 2.5,
         "color": WHITE, # (r,b,g) color value for the crosshair (or a predefined color from logic.py).
-        "width": 5,
+        "width": 4,
     },
     "volume": 0.5 # The volume of sounds, on a scale of 0 to 1.
 }
@@ -19,15 +19,19 @@ difficulty = {
     "undead": {
         "hp": 100,
         "speed": 1,
-        "wavetime": 10,     # The time between waves in seconds.
+        "wavetime": 5,     # The time between waves in seconds.
         "minwavesize": 2,   # Minimum zombies spawned per wave.
         "maxwavesize": 5,   # Maximum zombies spawned per wave.
+        "wavesizeinc": 5,   # By how much a wave's size can increase each round.
     },
     "gunship": {
         "firerate": 150,    # The delay between shots fired in milliseconds.
-        "dmgwidth": 50,     # The damage hitbox's width in pixels.
-        "dmg": 25,          # The damage of the weapon.     
+        "dmgwidth": 20,     # The damage hitbox's width in pixels.
+        "dmg": 20,          # The damage of the weapon.     
     },
+    "base": {
+        "hp": 20            # Unlike undead hp, base hp counts how many undead can make it to x = 0 (the left border) before the game is lost.
+    }
 }
 
 DEBUG = True
