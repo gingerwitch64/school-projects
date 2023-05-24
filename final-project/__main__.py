a329dff42b3e2f9d8dcddb317b040b652abcddcf
Nola G <redpendrew@gmail.com>
2023-05-24 13:17:36 -0400

    sync

--- final-project/__main__.py
+++ final-project/__main__.py
@@ -1,9 +1,14 @@
-import sys, pathlib, json
+import sys, argparse, pathlib, json
 # sys will be used to read command line arguments.
 from platform import python_version
 from datetime import datetime
+from pathlib import Path
 
-executed_from = pathlib.Path(__file__).parent.resolve()
+executed_from = Path(__file__).parent.resolve()
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("--pass", "-p", type=bool, help="Run the program without passing to the shell.")
+
 err_prefix = "Error"
 v = { # The version of this program
     "major": 0,
@@ -15,7 +20,7 @@ v_req = { # Python Version Requirements
     "minor": 4,
 }
 
-datetime_format = "%Y:%m:%d:%H:%M:%S %z"
+datetime_format = "%Y:%m:%d:%H:%M:%S" # Year:Month:Day:Hour:Minute:Second ; to be used for patch files
 
 default_indicators = {
     "same": "|",
@@ -24,17 +29,42 @@ default_indicators = {
     "comment": "&",
 }
 
-def parse_patch(filepath: type[pathlib.Path]):
+def parse_patch(filepath: type[Path]):
     with open(filepath,"r") as f:
         for line in f:
             print(line) # Placeholder!
 
-def write_patch(filepath: type[pathlib.Path]):    
+def write_patch(filepath: type[Path]):    
     pass
 
-def main():
+def main(argv = sys.argv, args = arg_parser.parse_args()):
+    # This first statement is the shell. The program will redirect to here if no arguments are given.
+    if len(argv) < 1 or (len(argv) < 2 and Path(argv[0]).resolve() == Path(__file__).resolve()):
+        print("No arguments given, passing off to built-in shell:")
+        shell = True
+        path = executed_from
+        help_text = [
+            "",
+            "patchi help menu",
+            "help | h - Prints this help text",
+            "quit | exit | q - Exits this shell",
+            "sysarg - Print arguments that were supplied to the program",
+            "",
+            ]
+        while shell:
+            command = input(f"{path}: ")
+            if command in {"quit","exit","q"}:
+                print("Quitting")
+                quit()
+            elif command in {"help","h"}:
+                for line in help_text: print(line)
+            elif command == "sysarg":
+                print(argv)
+            else:
+                for line in help_text: print(line)
     print(f"patchi version {v['major']}.{v['minor']}.{v['patch']}")
     print(datetime.now().strftime(datetime_format))
+    
 
 # Checks if python's version is greater than 3.4.x
 # This is to ensure compatability with pathlib.
