f39ae1248ae9c12704a18ea66eb58f298ed0d8f2
Nola G <redpendrew@gmail.com>
2023-05-24 10:17:41 -0400

    add some placeholders and fix version checker

--- final-project/__main__.py
+++ final-project/__main__.py
@@ -15,7 +15,7 @@ v_req = { # Python Version Requirements
     "minor": 4,
 }
 
-datetime_format = "%Y:%m:%d:%H:%M:%S"
+datetime_format = "%Y:%m:%d:%H:%M:%S %z"
 
 default_indicators = {
     "same": "|",
@@ -24,18 +24,23 @@ default_indicators = {
     "comment": "&",
 }
 
-def parse_patch(filepath: type[str]):
+def parse_patch(filepath: type[pathlib.Path]):
     with open(filepath,"r") as f:
         for line in f:
-            print(line)
+            print(line) # Placeholder!
+
+def write_patch(filepath: type[pathlib.Path]):    
+    pass
 
 def main():
+    print(f"patchi version {v['major']}.{v['minor']}.{v['patch']}")
     print(datetime.now().strftime(datetime_format))
 
 # Checks if python's version is greater than 3.4.x
 # This is to ensure compatability with pathlib.
-if (__name__ == "__main__") and (python_version().split(".")[0] == str(v_req["major"]) and python_version().split(".")[1] >= str(v_req["minor"])) or (python_version().split(".")[0] > str(v_req["major"])):
+if (__name__ == "__main__") and (int(python_version().split(".")[0]) == v_req["major"]) and (int(python_version().split(".")[1]) >= v_req["minor"]) or (int(python_version().split(".")[0]) > v_req["major"]):
     main()
 else:
     print(f"{err_prefix}: Python version must be at least {v_req['major']}.{v_req['minor']}")
+    print(f"You python version is {python_version()}")
     # A quit statement is not needed here; if the program does not meet the version requirement, it will quit anyhow (End of File)
